e98aa4a368a315b4ff40233b2b40042357e1bbdb
dodaroe <emanuele.dodaro@unical.it>
2026-09-21 14:58:33 +0200

    Remove deprecated event pages

--- openlab.php
+++
@@ -1,4 +0,0 @@
-<?php
-$page_slug = 'openlab'; // Inserisci qui l'identificativo URL della pagina
-require_once 'master_template.php'; 
-?>
--- welcomeweek_archivio.php
+++
@@ -1,4 +0,0 @@
-<?php
-$page_slug = 'welcomeweek'; // Inserisci qui l'identificativo URL della pagina
-require_once 'master_archivio.php'; 
-?>
